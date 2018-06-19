ffb1775f1d29b2dc99dc926353a0486d08226737
tagutomo <tsopq312726123@gmail.com>
2018-06-19 18:06:27 +0900

    練習

---
+++ tajiri/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 田尻宗準 on 2018/06/13.
+//  Copyright © 2018年 mune. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
